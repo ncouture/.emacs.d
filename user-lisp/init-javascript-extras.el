63fc6f29539dc2efd81f15b1aad9ca732e87faea
Nicolas Couture <nicolas@stormvault.net>
2025-04-20 05:07:09 -0400

    feat: update custom javascript extra code

--- user-lisp/init-javascript-extras.el
+++ user-lisp/init-javascript-extras.el
@@ -4,8 +4,7 @@
 
 (require-package 'lsp-mode)
 (require-package 'lsp-tailwindcss)
-(require-package 'vue3-mode)
-(require 'vue3-mode-autoloads)
+(require-package 'vue-mode)
 
 ;; Company fuzzy completion
 (when (and
@@ -14,8 +13,8 @@
   (company-flx-mode +1))
 
 ;; js2-jsx-mode
-(add-hook 'js2-jsx-mode-hook #'lsp-mode)
-(add-hook 'js2-jsx-mode-hook #'js2-jsx-indent-line)
+;; (add-hook 'js2-jsx-mode-hook #'lsp-mode)
+;; (add-hook 'js2-jsx-mode-hook #'js2-jsx-indent-line)
 
 ;; Vue configuration
 (setq session-use-package t)
@@ -23,18 +22,18 @@
 
 ;; This never worked
 ;;; (add-to-list 'auto-mode-alist '("\\.vue\\'" . envrc-allow))
-;;; (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
+;; (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
 ;;; (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode-reparse))
-(add-auto-mode 'lsp-mode "\\.\\(vue\\|vuex\\)\\'")
-(add-auto-mode 'lsp-tailwindcss-autoloads "\\.\\(vue\\|vuex\\)\\'")
+(add-auto-mode 'vue-mode "\\.vue")
+(add-auto-mode 'lsp-mode "\\.vue")
+(add-auto-mode 'lsp-tailwindcss "\\.\\vue")
+(add-auto-mode 'lsp-tailwindcss-rustywind "\\.vue")
 
 ;;; When mode is vue-mode set a before-save-hook to 'nil
 (add-hook 'vue-mode-hook
           (lambda ()
             (setq before-save-hook 'lsp-tailwindcss-rustywind-before-save)))
 
-(add-auto-mode 'lsp-mode "\\.\\(vue\\|vuex\\)\\'")
-;; (add-auto-mode 'lsp-tailwindcss-rustywind "\\.\\(vue\\|vuex\\)\\'")
 (setq lsp-completion-default-behaviour :insert)
 (setq lsp-completion-mode-hook '(lsp-tailwindcss-rustywind))
 
