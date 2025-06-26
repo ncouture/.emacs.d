65060c1b2489092824f5b9aee331ee66043e4f17
Nicolas Couture <nicolas@stormvault.net>
2025-06-26 08:27:20 -0400

    upate mcp servers removing twilio

--- user-lisp/init-mcp.el
+++ user-lisp/init-mcp.el
@@ -17,29 +17,14 @@
     ;; Read the first Lisp expression from the buffer
     (read (current-buffer))))
 
-(setq twilio-mcp-auth
-      (format "%s"
-              (read-value-from-file
-               (expand-file-name
-                "~/.ssh/twilip-mcp.txt"))))
+;; (setq twilio-mcp-auth
+;;       (format "%s"
+;;               (read-value-from-file
+;;                (expand-file-name
+;;                 "~/.ssh/twilip-mcp.txt"))))
 
 (setq mcp-hub-servers
-      `(("firebase" . (:command "npx" :args ("-y" "firebase-tools@latest" "experimental:mcp")))
-        ("twilio" . (:command "npx" :args
-                              ("-y"
-                               "@twilio-alpha/mcp"
-                               "expetimental/mcp"
-                               twilio-mcp-auth)))))
-                              ;; "--services"
-                              ;; "twilio_accounts_v1")))
-                              ;; ("filesystem" .
-                              ;;  (:command "npx" :args
-                              ;;            ("-y"
-                              ;;             "@model contextprotocol/server-filesystem"
-                              ;;             "/home/strmvlt/git/reparationbeton_web/functions")))
-                              ;; ("fetch" . (:command "uvx" :args ("mcp-server-fetch")))))))
-
-
+      `(("firebase" . (:command "npx" :args ("-y" "firebase-tools@latest" "experimental:mcp")))))
 (add-hook 'after-init-hook
           #'mcp-hub-start-all-server)
 
